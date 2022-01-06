089de712f914f8f9d6ffb432119f2e1bdb948f1d
marinahaugen <marina.santos.haugen@dnb.no>
2022-01-06 18:36:14 +0100

    Test push

--- Anagram/src/Anagram/Main.java
+++ Anagram/src/Anagram/Main.java
@@ -1,8 +1,36 @@
 package Anagram;
 
+import java.util.Arrays;
+
 public class Main {
 
+    static void checkStrings(String s1, String s2) {
+        s1.replaceAll("\\s", "");
+        s1.replaceAll("\\s", "");
+        boolean result = true;
+
+        if (s1.length() != s2.length()) {
+            result = false;
+        }
+        else {
+            char[] s1Ch = s1.toLowerCase().toCharArray();
+            char[] s2Ch = s2.toLowerCase().toCharArray();
+            Arrays.sort(s1Ch);
+            Arrays.sort(s2Ch);
+            result = Arrays.equals(s1Ch, s2Ch);
+        }
+
+        if(result) {
+            System.out.println("'" + s1 + "'" + " and " + "'" + s2 + "'" + " is an anagram.");
+        }
+        else {
+           System.out.println("'" + s1 + "'" + " and " + "'" + s2 + "'" + " is NOT an anagram.");
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Hello world!");
+	checkStrings("Hei på deg", "Hei på deg");
+	checkStrings("Hei på deg", "Nei nei hei");
     }
 }
